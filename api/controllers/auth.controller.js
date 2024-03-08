8ac5181107a188be2d26504a301c09c5a0f4995e
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-03-08 12:03:31 +0530

    create middlware to handles error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,15 @@
 import User from '../models/user.model.js';
 import brcypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup =async(req,res)=>{
+export const signup =async(req,res,next)=>{
     // console.log(req.body);
     const{ username ,email ,password } =req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === '') {
-        return  res.status(400).json({error:"All fields are required"})
+        next(errorHandler(400,"All fields are required"));
+        
+        // return  res.status(400).json({error:"All fields are required"})
     }
 
     const hashedPassword = await brcypt.hashSync(password,12);
@@ -20,7 +23,8 @@ export const signup =async(req,res)=>{
         await newUser.save();
         res.json('signup successfull')
     } catch (err) {
-        res.status(500).json({error:   `Something went wrong ===>${err.message}`})
+        next(err);
+        // res.status(500).json({error:   `Something went wrong ===>${err.message}`})
     }
 
 
--- api/index.js
+++ api/index.js
@@ -19,6 +19,16 @@ app.use(express.json())
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 
+app.use((err,req,res,next)=>{
+    const statusCode =err.statusCode || 500;
+    const message =err.message || 'Internal server error';
+    res.statusCode(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
