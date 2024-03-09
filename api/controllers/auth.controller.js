23ed22203d3282a717e8d3962328312a46fb3558
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-03-09 20:49:28 +0530

    sign in api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,33 +1,78 @@
-import User from '../models/user.model.js';
-import brcypt from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import User from "../models/user.model.js";
+import brcypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup =async(req,res,next)=>{
-    // console.log(req.body);
-    const{ username ,email ,password } =req.body;
+export const signup = async (req, res, next) => {
+  // console.log(req.body);
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === '' || email === '' || password === '') {
-        next(errorHandler(400,"All fields are required"));
-        
-        // return  res.status(400).json({error:"All fields are required"})
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+
+    // return  res.status(400).json({error:"All fields are required"})
+  }
+
+  const hashedPassword = brcypt.hashSync(password, 12);
+  const newUser = new User({
+    username: username.toLowerCase(),
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("signup successfull");
+  } catch (err) {
+    next(err);
+    // res.status(500).json({error:   `Something went wrong ===>${err.message}`})
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+    //   console.log("invalid 1");
+      return next(errorHandler(404, "Invalid User"));
+    //   console.log("invalid 2");
+    }
+    const validPassword = brcypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+    //   console.log("invalid 3");
+      return next(errorHandler(404, "Invalid password"));
+    //   console.log("invalid 4");
     }
 
-    const hashedPassword = await brcypt.hashSync(password,12);
-    const newUser = new User({
-        username :username.toLowerCase(),
-        email,
-        password:hashedPassword,   
-    }); 
+    // if everthing is create then auth,/token generate
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
 
-    try {
-        await newUser.save();
-        res.json('signup successfull')
-    } catch (err) {
-        next(err);
-        // res.status(500).json({error:   `Something went wrong ===>${err.message}`})
-    }
+    const { password: pass, ...rest } = validUser._doc;
 
-
-   
-}
-
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next("this went in catch part :: ", error);
+  }
+};
