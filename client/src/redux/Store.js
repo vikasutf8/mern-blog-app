9edea4f26b563857b0435bada882b4cb117ca604
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-03-10 16:12:35 +0530

    redux persist added as local storage

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,13 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+  user : userReducer,
+})
+
+const persistConfig={
+    key:'root',
+    storage,
+    version :1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user : userReducer,
-  },
+  reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 // export type RootState = ReturnType<typeof store.getState>
 // // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 // export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
