96a02026a2baa1d8b04e8d6ca4cc216b9ad10738
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-04-01 15:33:12 +0530

    created api delete route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,3 +47,16 @@ export const updateUser =async(req,res,next)=>{
             next(error)
         }
     }
+
+
+    export const deleteUser =async(req,res,next)=>{
+        if(req.user.id !== req.params.userId){
+            return next(errorHandler(403,"You are not authorized to delete this user"))
+        }
+        try {
+            await User.findByIdAndDelete(req.params.userId);
+            res.status(200).json("user hase been deleted")
+        } catch (error) {
+            next(error)
+        }
+    }
